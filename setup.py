c969485ed23d4bae69ea96eb6a8af144d42cadc7
Mr Fidal <n00b60d4r@gmail.com>
2025-12-23 01:41:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name="tornet",
-    version="2.3.0",
+    version="2.0.1",
     author="ByteBreach",
     author_email="mrfidal@proton.me",
     description="Advanced Tor Network Controller for IP Rotation and Anonymity",
